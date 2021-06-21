2f299fa6063d90709ce216817d3c72d80a76068a
Abdulbasit Gamoranao <basitgamor@gmail.com>
2021-06-22 00:18:34 +0800

    Add t-rows dynamically

---
+++ app.js
@@ -0,0 +1,52 @@
+// Basic ES5 OOP - constructors & prototypes
+
+// Book Constructor
+function Book(title, author, isbn) {
+  this.title = title;
+  this.author = author;
+  this.isbn = isbn;
+}
+// UI Constructor
+function UI() {}
+
+UI.prototype.addBookToList = function (book) {
+  const list = document.getElementById("book-list");
+  //create tr element
+  const row = document.createElement("tr");
+  // insert cols
+  row.innerHTML = `
+    <td>${book.title}</td>
+    <td>${book.author}</td>
+    <td>${book.isbn}</td>
+    <td><a href="#" class="delete">X</a></td>
+  `;
+  list.appendChild(row);
+};
+
+// Clear Fields
+UI.prototype.clearFields = function () {
+  document.getElementById("title").value = "";
+  document.getElementById("author").value = "";
+  document.getElementById("isbn").value = "";
+};
+// Event Listeners
+document.getElementById("book-form").addEventListener("submit", function (e) {
+  // get form values
+  const title = document.getElementById("title").value,
+    author = document.getElementById("author").value,
+    isbn = document.getElementById("isbn").value;
+
+  // Instantiate book
+  const book = new Book(title, author, isbn);
+
+  // Instantiate book
+  const ui = new UI();
+
+  // Add book to list
+  ui.addBookToList(book);
+
+  //clear fields
+  ui.clearFields();
+
+  e.preventDefault();
+});
